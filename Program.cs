0efa1fcf82c7450a63870be68fb30709725b4a37
James Larner <james.larner@aru.ac.uk>
2022-10-06 13:38:17 +0100

    Latest Version 0.1 Added Loop

--- Program.cs
+++ Program.cs
@@ -1,42 +1,46 @@
 ﻿int grade1, grade2, grade3, grade4, grade5, grade6;
 int average;
-
-try
+char choice;
+do
 {
-Console.WriteLine("Enter Grade1");
-grade1=Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter Grade2");
-grade2=Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter Grade3");
-grade3=Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter Grade4");
-grade4=Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter Grade5");
-grade5=Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter Grade6");
-grade6=Convert.ToInt32(Console.ReadLine());
+        try
+        {
+        Console.WriteLine("Enter Grade1");
+        grade1=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Grade2");
+        grade2=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Grade3");
+        grade3=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Grade4");
+        grade4=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Grade5");
+        grade5=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Grade6");
+        grade6=Convert.ToInt32(Console.ReadLine());
 
-average = (grade1+grade2+grade3+grade4+grade5+grade6)/6;
+        average = (grade1+grade2+grade3+grade4+grade5+grade6)/6;
 
-    switch (average)
-    {
-        case >=70:
-            Console.WriteLine("1st");
-            break;
+            switch (average)
+            {
+                case >=70:
+                    Console.WriteLine("1st");
+                    break;
 
-        case >=60:
-            Console.WriteLine("2:1");
-            break;
+                case >=60:
+                    Console.WriteLine("2:1");
+                    break;
 
-        default:
-            Console.WriteLine("Less than a 2:1");
-            break;
+                default:
+                    Console.WriteLine("Less than a 2:1");
+                    break;
 
-    }
-}
-catch
-{
-    Console.WriteLine("Incorrect Input");
-}
-
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Incorrect Input");
+        }
+        Console.WriteLine("Do you want to try again?");
+        choice=Convert.ToChar(Console.ReadLine());
+}while(choice != 'N');
 Console.ReadKey();
